f9dfc0b3ef92f8ea70110cbc7f3df662af7d9082
Gjergj Kadriu <96113511+gjergjkadriu@users.noreply.github.com>
2023-08-19 21:43:12 +0200

    working

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 require('dotenv').config();
 
+
 const indexRouter = require('./routes/index'); 
 const app = express();
 
 app.use(logger('dev'));
+app.use(require("cors")());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
